c05aea94ba35949cb485a5ee3685d8a1e8285529
Chad Smith <smith.chad12@gmail.com>
2023-11-05 00:12:19 -0500

    inital commit

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,18 @@
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+-- Directory navigation
+keymap.set("n", "<leader>m", ":NvimTreeFocus<CR>", opts)
+keymap.set("n", "<leader>f", ":NvimTreeToggle<CR>", opts)
+
+-- Navigation between panes
+keymap.set("n", "<C-h>", "<C-w>h", opts) -- Navigate left
+keymap.set("n", "<C-j>", "<C-w>j", opts) -- Navigate down
+keymap.set("n", "<C-k>", "<C-w>k", opts) -- Navigate Up
+keymap.set("n", "<C-l>", "<C-w>l", opts) -- Navigate Righ
+
+-- Window Managment
+keymap.set("n", "<leader>sv", ":vsplit<CR>", opts) -- Split Vertically
+keymap.set("n", "<leader>sh", ":split<CR>", opts) -- Split horizontally
+keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>", opts) -- Split minimize
+
